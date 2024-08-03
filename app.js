88c0de397b14b5974037fb7bcb16f645266dda1d
gustianaeko <geko477@gmail.com>
2024-08-03 16:04:37 +0700

    add handlebar, create routes (still only method get)

--- app.js
+++ app.js
@@ -2,10 +2,33 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("kok belum otomatis");
-});
+//view engine
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+//static file access
+app.use("/assets", express.static("assets"));
+
+//body parser (parse from string to obj)
+app.use(express.json());
+
+//routes
+const renderIndex = (req, res) => {
+  res.render("index");
+};
+
+const renderAddProject = (req, res) => {
+  res.render("addProject");
+};
+
+const renderContactMe = (req, res) => {
+  res.render("contactMe");
+};
+
+app.get("/", renderIndex);
+app.get("/addProject", renderAddProject);
+app.get("/contactMe", renderContactMe);
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port} `);
+  console.log(`App listening on port ${port}`);
 });
